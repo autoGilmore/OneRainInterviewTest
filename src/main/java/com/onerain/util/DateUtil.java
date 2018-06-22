b905648c50f865688b09e8b82348dba2a7949cd6
Adrian <abgilmore@gmail.com>
2018-06-22 12:47:12 -0600

    Add custom message for days until the new year. Fix wrong number of days in DateUtil dayOfYear method for January. Fix missing leap year additional day calculation in the DateUtil dayOfYear method. Tests passing.

--- src/main/java/com/onerain/util/DateUtil.java
+++ src/main/java/com/onerain/util/DateUtil.java
@@ -3,6 +3,9 @@ package com.onerain.util;
 public class DateUtil {
 
 	public static int dayOfYear(int month, int dayOfMonth, int year) {
+
+		// TODO: magic numbers for days in month. Create constants or get from
+		// Calendar.
 		if (month == 2) {
 			dayOfMonth += 31;
 		} else if (month == 3) {
@@ -24,8 +27,21 @@ public class DateUtil {
 		} else if (month == 11) {
 			dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
 		} else if (month == 12) {
-			dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 31;
+			dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
 		}
+
+		// add leap day
+		if (isLeapYear(year) && month > 2)
+			dayOfMonth += 1;
+
 		return dayOfMonth;
+	}
+
+	private static boolean isLeapYear(int year) {
+		// copied from:
+		// https://stackoverflow.com/questions/725098/leap-year-calculation
+		if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+			return true;
+		return false;
 	}
 }
